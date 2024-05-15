fb1586cec7c7567b6139d81d679f1a96e5d1286a
soumyaaaaaaaaaaaaaaaaaaaaaaa <167617017+soumyaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-15 16:48:40 +0530

    relational operators

---
+++ src/conditionall/Conditionall.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conditionall;
+
+/**
+ *
+ * @author dell1
+ */
+public class Conditionall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int a=5,b=15,c=4;
+        
+        if(a>b && a>c)
+        {
+            System.out.println(a);
+        }
+        else if(b>c)
+        {
+            System.out.println(b);
+        }
+        else
+        {
+            System.out.println(c);
+    }
+    
+}
+}
